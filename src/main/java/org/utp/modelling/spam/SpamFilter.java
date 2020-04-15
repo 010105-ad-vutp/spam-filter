18d3fa32f6e2873f2185abc02e2c1688fc0b7204
Alex Delchev <alexdelchev97@gmail.com>
2020-04-15 23:10:05 +0300

    Add main class

---
+++ src/main/java/org/utp/modelling/spam/SpamFilter.java
@@ -0,0 +1,13 @@
+package org.utp.modelling.spam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SpamFilter {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpamFilter.class, args);
+  }
+}
